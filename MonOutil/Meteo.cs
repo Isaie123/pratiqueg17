85eb2e4bd043ac4ada048d1ab735861acbb441e5
Isaie Bourget <isaie.bourget@lcieducation.net>
2023-09-19 10:14:55 -0400

    Translate in English

---
+++ MonOutil/Meteo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonOutil
+{
+    internal class Meteo
+    {
+        public Meteo()
+        {
+            double tempEnCelsius;
+            double tempEnFarhrenheit;
+            string? celsius;
+
+            Console.WriteLine("Welcome to my wheater program");
+            Console.WriteLine("Enter the temperature in Celsius : ");
+            celsius = Console.ReadLine();
+            tempEnCelsius = Convert.ToDouble(celsius);
+            tempEnFarhrenheit = (tempEnCelsius * 9 / 5) + 32;
+            Console.WriteLine($"La température en Fahrenheit: {tempEnFarhrenheit}");
+        }
+
+            
+            
+            
+            
+           
+
+    }
+}
---
+++ MonOutil/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MonOutil;
+
+Console.WriteLine("Bonjour, quel est votre nom");
+
+string? usernames;
+usernames = Console.ReadLine();
+
+Console.WriteLine($"Bienvenu {usernames} à MonOutil.");
+
+new Meteo();
